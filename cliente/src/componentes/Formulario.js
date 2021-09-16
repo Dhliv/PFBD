cd1462f7a24601032e1dc03c0594904d54070c04
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-16 11:57:04 -0500

    .

--- cliente/src/componentes/Formulario.js
+++ cliente/src/componentes/Formulario.js
@@ -1,29 +1,30 @@
 import React from "react";
-import {Pregunta} from "./Pregunta"
-import { Respuesta} from "./Respuesta";
+import { Pregunta } from "./Pregunta"
+import { Respuesta } from "./Respuesta";
+import { questions } from "../json/questions.json";
 
 // Create a new form, then add a checkbox question, a multiple choice question,
 // a page break, then a date question and a grid of questions.
 
 
 const Formulario = (props) => {
-    
-    let preguntas = props.preguntas;
-    // props = [[preguntas,respuestas]...[preguntas,respuestas]]
 
-    return(
-        <section>
-            <ol>
-            {preguntas.map(preguntaYRespuesta =>
-                <li>
-                    <Pregunta key={preguntaYRespuesta[0]} pregunta={preguntaYRespuesta[0]}/>
-                    <Respuesta respuestas={preguntaYRespuesta.splice(1,preguntaYRespuesta.length-1)}/>
-                </li>
-                )}
-        
-            </ol>
-        </section>
-    )
+  let preguntas = questions;
+  // props = [[preguntas,respuestas]...[preguntas,respuestas]]
+
+  return (
+    <section>
+      <ol>
+        {preguntas.map(preguntaYRespuesta =>
+          <li>
+            <Pregunta key={preguntaYRespuesta[0]} pregunta={preguntaYRespuesta[0]} />
+            <Respuesta respuestas={preguntaYRespuesta.splice(1, preguntaYRespuesta.length - 1)} />
+          </li>
+        )}
+
+      </ol>
+    </section>
+  )
 };
 
-export {Formulario};
+export { Formulario };
