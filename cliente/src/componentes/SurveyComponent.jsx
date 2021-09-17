9c0cc01d80e37c66e5ba27533af63f08ccbdd4ca
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-16 19:46:45 -0500

    Se colocaron todas las preguntas en la página.

--- cliente/src/componentes/SurveyComponent.jsx
+++ cliente/src/componentes/SurveyComponent.jsx
@@ -4,50 +4,40 @@ import * as Survey from "survey-react";
 
 import "survey-react/survey.css";
 import "./index.css";
-import json2 from "../json/neoQ.json";
+import preguntas from "../json/neoQ.json";
 import { questions } from "../json/questions.json"
 
 Survey.StylesManager.applyTheme("default");
 
-var tipoPregunta = ["checkbox", "radiogroup", "radiogroup", "checkbox"];
+var tipoPregunta = ["checkbox", "radiogroup", "radiogroup", "checkbox", "text"];
 
 class SurveyComponent extends Component {
   constructor() {
     super();
   }
 
-  addQuestion(tipo, pos) {
-    const valorDado = "funcionará?";
-    var json = [
+  addQuestion(posTitulo, posPregunta) {
+    const tipo = tipoPregunta[questions[posTitulo].tipoPregunta];
+    const titulo = questions[posTitulo].titulo;
+    const respuesta = posPregunta != -1 ? questions[posTitulo].preguntas[posPregunta] : "";
+    const nombre = posPregunta != -1 ? titulo + " " + respuesta : titulo;
+    const nCol = questions[posTitulo].respuestas.length;
+    const choices = [];
+
+    for (let i = 0; i < nCol; i++) {
+      choices.push(questions[posTitulo].respuestas[i][0]);
+    }
+
+    let json = [
       {
         type: tipo,
-        name: questions[pos].titulo,
-        title: questions[pos].titulo,
+        name: nombre,
+        title: posPregunta != -1 ? respuesta : titulo,
         isRequired: true,
-        hasNone: true,
-        colCount: 4,
-        rateValues: [
-          { value: 1, text: valorDado },
-          { value: 2, text: "No Contesta" },
-          { value: 3, text: "0" },
-          { value: 4, text: "1" },
-          { value: 5, text: "2" },
-          { value: 6, text: "3" },
-          { value: 7, text: "4" },
-          { value: 8, text: "5" }
-        ],
-        choices: [
-          "Ford",
-          "Vauxhall",
-          "Volkswagen",
-          "Nissan",
-          "Audi",
-          "Mercedes-Benz",
-          "BMW",
-          "Peugeot",
-          "Toyota",
-          "Citroen"
-        ]
+        inputType: "number",
+        hasNone: false,
+        colCount: Math.max(1, Math.floor(nCol / 2)),
+        choices: choices
       }
     ];
 
@@ -56,10 +46,17 @@ class SurveyComponent extends Component {
 
   render() {
 
-    json2.questions = json2.questions.concat(this.addQuestion("rating", 0));
-    json2.questions = json2.questions.concat(this.addQuestion("checkbox", 1));
-    json2.questions = json2.questions.concat(this.addQuestion("radiogroup", 2));
-    const survey = new Survey.Model(json2);
+    for (let i = 0; i < questions.length; i++) {
+      let nP = questions[i].tipoPregunta;
+      if (nP == 2 || nP == 3) {
+        for (let j = 0; j < questions[i].preguntas.length; j++) {
+          preguntas.questions = preguntas.questions.concat(this.addQuestion(i, j));
+        }
+      } else {
+        preguntas.questions = preguntas.questions.concat(this.addQuestion(i, -1));
+      }
+    }
+    const survey = new Survey.Model(preguntas);
 
     return <Survey.Survey model={survey} />;
   }
