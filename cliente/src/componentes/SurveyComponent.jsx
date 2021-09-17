faa7ead80773159b8d3ae74e7f157c581724a5f5
perqueza72 <practica22correo@gmail.com>
2021-09-17 15:57:59 -0500

    Cambios visuales y orden en las preguntas multiples

--- cliente/src/componentes/SurveyComponent.jsx
+++ cliente/src/componentes/SurveyComponent.jsx
@@ -4,62 +4,138 @@ import * as Survey from "survey-react";
 
 import "survey-react/survey.css";
 import "./index.css";
-import preguntas from "../json/neoQ.json";
+import neoQ from "../json/neoQ.json";
 import { questions } from "../json/questions.json"
 
 Survey.StylesManager.applyTheme("default");
 
 var tipoPregunta = ["checkbox", "radiogroup", "radiogroup", "checkbox", "text"];
+var titulo;
+const CHECKBOX = 3;
+const CHECKBOX2 = 2;
 
-class SurveyComponent extends Component {
-  constructor() {
-    super();
+function addQuestion(posTitulo, posPregunta) {
+  
+  const tipo = tipoPregunta[questions[posTitulo].tipoPregunta];
+  titulo = questions[posTitulo].titulo;
+  const respuesta = posPregunta !== -1 ? questions[posTitulo].preguntas[posPregunta] : "";
+  const nombre = posPregunta !== -1 ? titulo + " " + respuesta : titulo;
+  const nCol = questions[posTitulo].respuestas.length;
+  const choices = [];
+
+  for (let i = 0; i < nCol; i++) {
+    choices.push(questions[posTitulo].respuestas[i][0]);
   }
 
-  addQuestion(posTitulo, posPregunta) {
-    const tipo = tipoPregunta[questions[posTitulo].tipoPregunta];
-    const titulo = questions[posTitulo].titulo;
-    const respuesta = posPregunta != -1 ? questions[posTitulo].preguntas[posPregunta] : "";
-    const nombre = posPregunta != -1 ? titulo + " " + respuesta : titulo;
-    const nCol = questions[posTitulo].respuestas.length;
-    const choices = [];
-
-    for (let i = 0; i < nCol; i++) {
-      choices.push(questions[posTitulo].respuestas[i][0]);
+  let question = [
+    {
+      type: tipo,
+      name: nombre,
+      title: posPregunta !== -1 ? respuesta : titulo,
+      isRequired: false,
+      inputType: "number",
+      hasNone: false,
+      colCount: Math.max(1, Math.floor(nCol / 2)),
+      choices: choices
     }
+  ];
 
-    let json = [
-      {
-        type: tipo,
-        name: nombre,
-        title: posPregunta != -1 ? respuesta : titulo,
-        isRequired: true,
-        inputType: "number",
-        hasNone: false,
-        colCount: Math.max(1, Math.floor(nCol / 2)),
-        choices: choices
+  return question;
+}
+
+
+function createJson(){
+
+  var aux = {
+    title: titulo,
+    questions: []
+  };
+  
+
+  for (let i = 0; i < questions.length; i++) {
+    let nP = questions[i].tipoPregunta;
+    if (nP === CHECKBOX || nP === CHECKBOX2) {
+
+      for (let j = 0; j < questions[i].preguntas.length; j++) {
+        aux.questions = aux.questions.concat(addQuestion(i, j));
       }
-    ];
-
-    return json;
-  }
-
-  render() {
-
-    for (let i = 0; i < questions.length; i++) {
-      let nP = questions[i].tipoPregunta;
-      if (nP == 2 || nP == 3) {
-        for (let j = 0; j < questions[i].preguntas.length; j++) {
-          preguntas.questions = preguntas.questions.concat(this.addQuestion(i, j));
-        }
-      } else {
-        preguntas.questions = preguntas.questions.concat(this.addQuestion(i, -1));
-      }
+      neoQ.pages = neoQ.pages.concat(aux);
+      
+      aux = {
+        title: titulo,
+        questions: []
+      };
+    } else {
+      
+      aux.questions = aux.questions.concat(addQuestion(i, -1));
+      neoQ.pages = neoQ.pages.concat(aux);
+      
+      aux = {
+        title: titulo,
+        questions: []
+      };
     }
-    const survey = new Survey.Model(preguntas);
-
-    return <Survey.Survey model={survey} />;
   }
 }
 
-export default SurveyComponent;
+function SurveyComponent() {
+
+  createJson();
+  const survey = new Survey.Model(neoQ);
+  console.log(neoQ);
+
+    return(
+      <Survey.Survey model={survey} />
+      );
+}
+
+export {SurveyComponent};
+
+
+
+/**
+ * var json = {
+    questionTitleLocation: "top",
+    showQuestionNumbers: "on",
+    pages: [
+        {
+            name: "Address",
+            title: "Address",
+            questions: [
+                {
+                    type: "text",
+                    name: "address1",
+                    title: "Street Address"
+                }, {
+                    type: "text",
+                    name: "address2",
+                    title: "Address Line 2"
+                }, {
+                    type: "text",
+                    name: "city",
+                    title: "City"
+                }, {
+                    type: "text",
+                    name: "state",
+                    startWithNewLine: false,
+                    title: "State / Province / Region"
+                }, {
+                    type: "text",
+                    name: "zip",
+                    title: "Zip / Postal Code"
+                }, {
+                    type: "dropdown",
+                    name: "country",
+                    startWithNewLine: false,
+                    title: "Country",
+                    choicesByUrl: {
+                        url: "https://restcountries.eu/rest/v2/all",
+                        valueName: "name"
+                    }
+                }
+            ]
+        }
+    ]
+};
+ * 
+ */
--- cliente/src/index.js
+++ cliente/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Formdb } from './Formdb';
-import SurveyComponent from './componentes/SurveyComponent';
+import {SurveyComponent} from './componentes/SurveyComponent';
 
 import './styles/styles.scss'
 
