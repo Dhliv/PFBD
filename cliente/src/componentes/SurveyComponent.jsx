c99bb04e88fd78620d9b47c9d0a3a71290af71fb
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-22 21:16:11 -0500

    WIP: Obtener del JSON las respuestas de la encuesta.

--- cliente/src/componentes/SurveyComponent.jsx
+++ cliente/src/componentes/SurveyComponent.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-
 import * as Survey from "survey-react";
-
 import "survey-react/survey.css";
 import neoQ from "../json/neoQ.json";
+import { checkAllQuestions } from "./parseUserRespToBDFormat";
 
 Survey.StylesManager.applyTheme("bootstrap");
 Survey.defaultBootstrapCss.navigationButton = "btn btn-red";
@@ -14,10 +13,6 @@ var surveyValueChanged = function (sender, options) {
     el.value = options.value;
   }
 };
-
-function parserUserRespToBDFormat() {
-
-}
 
 /**
  * Crea la encuesta y retorna el componente jsx grafico.
@@ -31,8 +26,9 @@ function SurveyComponent() {
   survey
     .onComplete
     .add(function (sender) {
-
-      console.log(JSON.stringify(sender.data, null, 2));
+      let ans = sender.data;
+      //checkAllQuestions(sender.data);
+      console.log(ans.valueOf("Indique su género"), typeof ans);
     });
 
   return (
--- cliente/src/index.js
+++ cliente/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { SurveyComponent } from './componentes/SurveyComponent';
 import { JSONWithQuestions } from './componentes/JSONWithQuestions';
 import './styles/styles.scss'
-import questions from "./json/questions.json"
 
-console.log(questions.tipado === undefined)
 JSONWithQuestions.init()
 ReactDOM.render(<SurveyComponent />, document.getElementById('root'))
