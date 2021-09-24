e5e72937fa2c0b196a407cb642bd3e8431a44c21
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-23 21:38:53 -0500

    bug fixes.

--- cliente/src/componentes/SurveyComponent.jsx
+++ cliente/src/componentes/SurveyComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as Survey from "survey-react";
 import "survey-react/survey.css";
 import neoQ from "../json/neoQ.json";
-import { checkAllQuestions } from "./parseUserRespToBDFormat";
+import { checkAnswers } from "./parseUserRespToBDFormat";
 
 Survey.StylesManager.applyTheme("bootstrap");
 Survey.defaultBootstrapCss.navigationButton = "btn btn-red";
@@ -26,7 +26,7 @@ function SurveyComponent() {
   survey
     .onComplete
     .add(function (sender) {
-      checkAllQuestions(sender.data);
+      checkAnswers(sender.data);
     });
 
   return (
