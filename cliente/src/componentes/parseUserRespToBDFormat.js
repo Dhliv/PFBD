b4b80a4fd790b1b793e133863625356977403155
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-23 21:39:30 -0500

    Parseo semi-completo de todas las respuestas (Solo hace parse para la tabla de "resultado_preguntas".

--- cliente/src/componentes/parseUserRespToBDFormat.js
+++ cliente/src/componentes/parseUserRespToBDFormat.js
@@ -1,17 +1,59 @@
 import { categorias, puntaje } from "./JSONWithQuestions"
+import { Registroinfo } from "../Registroinfo";
+import insertIntoTable from "../json/insertIntoTable.json";
 
-function checkAllQuestions(answers) {
-  let question;
-  let answer;
-  for (let key in answers) {
-    question = String(key);
-    answer = String(answers[key]);
-    if (categorias.get(question) !== undefined) {
-      console.log(categorias.get(question));
-    } else {
-      console.log(categorias.get(question + " " + answer));
+var categories;
+var score;
+var idUsuario;
+var idRespuesta;
+
+function parseAnswerForBD() {
+  let template = {
+    table: "",
+    info: []
+  }
+
+  let aux;
+  let category;
+
+  for (let i = 0; i < categories.length; i++) {
+    category = categories[i];
+    aux = JSON.parse(JSON.stringify(template, null, 2)); // Deep copy of template.
+    if (category > 0 && category <= 9) { // La respuesta va en la tabla de respuestas.
+      idRespuesta = Registroinfo.idRespuesta();
+
+      aux.table = "resultado_preguntas";
+      aux.info.push(idRespuesta);
+      aux.info.push(category);
+      aux.info.push(score);
+
+    } else { // La respuesta va en otra parte.
+
     }
+    insertIntoTable.toInsert = insertIntoTable.toInsert.concat(aux);
   }
 }
 
-export { checkAllQuestions };
+/**
+ * Obtiene todas las respuestas a la survey y les aplica el formato para inserción en la BD.
+ * @param {JSON} answers respuestas de la survey.
+ */
+function checkAnswers(answers) {
+  let question;
+  let answer;
+  idUsuario = Registroinfo.idUsuario();
+
+  for (let key in answers) {
+    question = String(key);
+    answer = String(answers[key]);
+    score = puntaje.get(answer);
+
+    if (categorias.get(question) !== undefined) categories = categorias.get(question);
+    else categories = categorias.get(question + " " + answer);
+    parseAnswerForBD();
+  }
+
+  console.log(insertIntoTable.toInsert);
+}
+
+export { checkAnswers };
