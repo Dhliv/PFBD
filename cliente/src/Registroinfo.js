264ff809aa5bc19cd727b6c49d786b26632b06ec
David Henao <david.henao.martinez@correounivalle.edu.co>
2021-09-27 19:05:10 -0500

    Se inserta un usuario y se retorna su id.

--- cliente/src/Registroinfo.js
+++ cliente/src/Registroinfo.js
@@ -13,41 +13,17 @@ class Registroinfo {
       const edad = insertIntoTable.toInsert[userPos].info[1];
       const gastoSemanal = insertIntoTable.toInsert[userPos].info[2];
       const conQuienSale = (await axios.post('basedatos/getIdConQuienSale', { "conQuienSale": insertIntoTable.toInsert[userPos].info[3] })).data.idconQuienSale;
-      const ocupacion = await (await axios.post('basedatos/getIdOcupacion', { "ocupacion": insertIntoTable.toInsert[userPos].info[4] })).data.idOcupacion;
-      const estudio = await (await axios.post('basedatos/getIdEstudio', { "estudio": insertIntoTable.toInsert[userPos].info[5] })).data.idEstudio;
+      const ocupacion = (await axios.post('basedatos/getIdOcupacion', { "ocupacion": insertIntoTable.toInsert[userPos].info[4] })).data.idOcupacion;
+      const estudio = (await axios.post('basedatos/getIdEstudio', { "estudio": insertIntoTable.toInsert[userPos].info[5] })).data.idEstudio;
       console.log(genero, edad, gastoSemanal, conQuienSale, ocupacion, estudio);
-      //const idUsuario = await axios.post('basedatos/insertUsuario', { "genero": user[0], "edad": user[0], "gastoSemanal": user[0], "conQuienSale": user[0], "ocupacion": user[0], "estudio": user[0] });
+      const idUsuario = (await axios.post('basedatos/insertUsuario', {
+        "genero": genero, "edad": edad, "gastoSemanal": gastoSemanal, "conQuienSale": conQuienSale, "ocupacion": ocupacion, "estudio": estudio
+      }
+      )).data.idUsuario;
+      console.log(idUsuario);
     }
 
     doAll();
-  }
-
-  /**
-   * Obtiene el siguiente valor de idRespuesta de la BD y lo almacena.
-   */
-  static idRespuesta(table, category, score) {
-    const idR = async () => {
-      // const res = await axios.post('/basedatos/getNewIdRespuesta', {});
-      // let id = res.data.idRespuesta;
-      // let data = [id, category, score];
-      // saveParsedAnswer(table, data);
-    }
-
-    idR();
-  }
-
-  /**
-   * Obtiene el siguiente valor de idUsuario de la BD y lo almacena.
-   */
-  static idUsuario() {
-    let id;
-    const idUsuario = async () => {
-      const res = await axios.post('/basedatos/getNewIdUsuario', {});
-      id = res.data.idUsuario;
-    }
-
-    idUsuario();
-    return id;
   }
 
   static getScoresByEvent() {
--- routers/basedatos.js
+++ routers/basedatos.js
@@ -35,7 +35,7 @@ router.post('/insertUsuario', async (req, res) => {
   const idUsuario = await client.query(
     `insert into usuario(id_genero, id_estudio, id_ocupacion, id_con_quien_sale, edad, gasto_semanal_ocio) values(${genero}, ${estudio}, ${ocupacion}, ${conQuienSale}, ${edad}, ${gastoSemanal}) returning id_usuario;`
   );
-  res.send({ "idUsuario": idUsuario.rows[0].nextval });
+  res.send({ "idUsuario": idUsuario.rows[0].id_usuario });
 });
 
 /**
